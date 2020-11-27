7cdd913fff588a2f09f95430fbdf7aad6207a865
mrhuangyuhui <mrhuangyuhui@gmail.com>
2020-11-27 18:31:09 +0800

    temp

--- cal1.py
+++ cal1.py
@@ -5,7 +5,7 @@ getcontext().prec = 6
 money = Decimal(300)
 print("交易总额", money)
 
-n = 6
+n = 5
 print("交易次数", n)
 
 per_money = Decimal(money) / Decimal(n)
@@ -15,7 +15,7 @@ base_price = Decimal(18000)
 print("进场价格", base_price)
 
 interval = Decimal("0.01")
-print("交易间隔", interval)
+print("交易间隔", Decimal(interval) * Decimal(100), "%")
 
 print("----------")
 
@@ -32,14 +32,15 @@ for i in range(n):
     print("仓位资金", sum_money)
 
     baocang_distance = Decimal(money) / Decimal(sum_money)
-    print("平均价格与爆仓价格距离（百分比）", baocang_distance)
+    print("平均价格与爆仓价格距离（百分比）", baocang_distance, "%")
     
     # 平均成本与爆仓价格的距离 
     # 计算公式：平均成本 * (100 - 爆仓幅度) / 100
-    baocang_price = Decimal(average_price) * ( Decimal(100) - Decimal(baocang_distance) ) / Decimal(100)
+    # baocang_price = Decimal(average_price) * ( Decimal(100) - Decimal(baocang_distance) ) / Decimal(100)
+    baocang_price = Decimal(average_price) * ( 1 - ( Decimal(baocang_distance) / Decimal(100) ) )
     print("爆仓价格", baocang_price)
 
     # 计算公式：当前价格 * (100 - 爆仓幅度) / 100
     current_price_distance = Decimal(1) - Decimal(baocang_price) / Decimal(buy_price)
-    print("当前价格与爆仓价格距离（百分比）", Decimal(current_price_distance) * Decimal(100))
+    print("当前价格与爆仓价格距离（百分比）", Decimal(current_price_distance) * Decimal(100), "%")
     print("----------")
